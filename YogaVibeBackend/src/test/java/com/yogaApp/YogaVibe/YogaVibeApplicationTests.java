68e7111b38fd1494ed21c92d56332328e5f3ac67
Anshul <anshulsen01032006@gmail.com>
2026-04-14 14:51:34 +0530

    added backend folder

---
+++ YogaVibeBackend/src/test/java/com/yogaApp/YogaVibe/YogaVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogaApp.YogaVibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaVibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
